4f6499d9181126376a4cb4459e51308111a54134
RockPiryt <rockpiryt@gmail.com>
2023-06-13 18:37:57 +0200

    'RockPiryt'- add new templates, add new route
    Add templates:
    - footer template
    - header template
    - portfolio template
    - info_project template
    
    Add route:
    - route(/info)

--- main.py
+++ main.py
@@ -6,6 +6,9 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")
 
+@app.route('/info')
+def info_project():
+    return render_template("info_project.html")
 
 if __name__ == "__main__":
     app.run(debug=True, host="localhost", port="5000")
