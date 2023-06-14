00cea96e4ec6572b50724c4ff92a129207e52600
RockPiryt <rockpiryt@gmail.com>
2023-06-14 09:56:52 +0200

    'RockPiryt' - create forms and send email with information.
    - Create WTF forms with validation.
    - Use smtplib to get information from contact form and send by email.

--- main.py
+++ main.py
@@ -1,6 +1,19 @@
 from flask import Flask, render_template
+from forms import ContactForm
+import smtplib
+from email.message import EmailMessage
+import os
+from dotenv import load_dotenv
+
+#Get user info to send email
+load_dotenv("C:/Users/Popuś/Desktop/Python/environment_variables/.env")
+my_email= os.getenv("MY_EMAIL")
+api_key_gmail = os.getenv("APP_PASSWORD_GMAIL")
+
 
 app = Flask(__name__)
+app.secret_key = "myPass1234!!"
+app.config["SECRET_KEY"]= "myPass1234!!"
 
 @app.route('/')
 def home():
@@ -9,6 +22,48 @@ def home():
 @app.route('/info')
 def info_project():
     return render_template("info_project.html")
+
+@app.route('/contact', methods=["GET", "POST"])
+def user_contact():
+    form_python = ContactForm()
+
+    if form_python.validate_on_submit():
+        user_name = form_python.name.data
+        user_email = form_python.email.data
+        user_subject = form_python.subject.data
+        user_message = form_python.message.data
+
+        user_info = {
+            "name": user_name,
+            "email": user_email,
+            "subject": user_subject,
+            "message": user_message,
+        }
+
+        send_email(user_name, user_email, user_subject, user_message)
+        
+        return render_template("get_info.html", html_user_info=user_info)
+    return render_template("contact_page.html", html_form=form_python)
+
+def send_email(name, email, subject, message):
+    #Create email
+    user_info = f"""
+    Here are information from user: \n
+    Name: {name}, \n
+    Email: {email}, \n
+    Message: {message}.
+    """
+    msg = EmailMessage()
+    msg.set_content(user_info)
+    msg["Subject"] = f"{subject}"
+    msg["From"] = my_email
+    msg["To"] = my_email
+
+    
+    with smtplib.SMTP("smtp.gmail.com") as connection:
+        connection.starttls()
+        connection.login(user=my_email, password=api_key_gmail)
+        connection.send_message(msg)
 
 if __name__ == "__main__":
     app.run(debug=True, host="localhost", port="5000")
